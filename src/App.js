507240d7e16ae4838ae0221a315e9e29dfd0f694
Rasel91 <srasel.dpe@gmail.com>
2022-10-07 22:31:30 +0600

    first commit

---
+++ src/App.js
@@ -0,0 +1,13 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      <h1 className='text-4xl font-bold '> This is my price line</h1>
+      <p> This is my price line</p>
+    </div>
+  );
+}
+
+export default App;
