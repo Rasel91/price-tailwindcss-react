31789afdd481488b6cac624b7febc6e23b1f6c9a
Rasel91 <srasel.dpe@gmail.com>
2022-10-07 23:15:29 +0600

    navbar add

--- src/App.js
+++ src/App.js
@@ -1,9 +1,11 @@
-import logo from './logo.svg';
+
 import './App.css';
+import NavBar from './components/NavBar';
 
 function App() {
   return (
     <div className="App">
+      <NavBar></NavBar>
       <h1 className='text-4xl font-bold '> This is my price line</h1>
       <p> This is my price line</p>
     </div>
---
+++ src/components/NavBar.js
@@ -0,0 +1,26 @@
+import React from 'react';
+import Link from './Link';
+
+const NavBar = () => {
+ const routes=[
+    {id:1, name:'Home', path:'/home'},
+    {id:2, name:'Products', path:'/products'},
+    {id:3, name:'Orders', path:'/Orders'},
+    {id:4, name:'Contact', path:'/contact'},
+    {id:5, name:'About', path:'/about'},
+ ]
+
+    return (
+
+        <nav>
+            <ul  className='md:flex justify-center '>
+                {
+                 routes.map(route => <Link key={route.id} route={route}></Link>)
+                }
+            </ul>
+            
+        </nav>
+    );
+};
+
+export default NavBar;
